b0c4c33994201cf013d28b918e3544c14d86e559
Thirupathi2007 <thirupathisarathi2007@gmail.com>
2026-04-10 11:11:01 +0530

    Time: 1 ms (38.54%), Space: 42.4 MB (54.14%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestNumber(int n) {
+        int k = 1;
+        
+        while (true) {
+            int x = (1 << k) - 1; // 2^k - 1
+            
+            if (x >= n) {
+                return x;
+            }
+            
+            k++;
+        }
+    }
+}
